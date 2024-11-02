2fd184df405b954ef4c7100daf598bd4391bfa5c
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-11-02 19:00:35 -0400

    codewars strings

---
+++ main.js
@@ -0,0 +1,5 @@
+function check(a, x) {
+    // array is given - check if character is in the array
+    //use includes to check it will be true if not it will return false. 
+    return a.includes(x)
+  }
